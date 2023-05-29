27287da359e10530fc3f5d05784a8f102d81c9b3
Luiz Fernando <luiz.s08@aluno.ifsc.edu.br>
2023-05-29 08:50:48 -0300

    Commits iniciais
    Commits iniciais feitos na aula 29/05, com arquivos .sql, .png e .mwb

---
+++ scripts_curintia.sql
@@ -0,0 +1,93 @@
+-- Excliui o banco de dados
+-- DROP DATABASE curintia IF EXISTS;
+
+-- CRIA O BANCO DE DADOS
+CREATE DATABASE IF NOT EXISTS curintia;
+
+-- USA O BANCO DE DADOS curiintia
+USE curintia;
+
+CREATE TABLE IF NOT EXISTS `enderecos` (
+  `cep` BIGINT NOT NULL,
+  `rua` VARCHAR(45) NOT NULL,
+  `bairro` VARCHAR(45) NOT NULL,
+  `cidade` VARCHAR(45) NOT NULL,
+  `estado` VARCHAR(45) NOT NULL,
+  `complemento` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`cep`)
+);
+
+CREATE TABLE IF NOT EXISTS `fornecedores` (
+  `id_fornecedor` BIGINT NOT NULL,
+  `nome` VARCHAR(45) NOT NULL,
+  `endereco` VARCHAR(45) NOT NULL,
+  `cnpj` BIGINT NOT NULL,
+  `telefone` BIGINT NOT NULL,
+  `empresa` VARCHAR(45) NOT NULL,
+  `endereco_cep` BIGINT NOT NULL,
+  PRIMARY KEY (`id_fornecedor`),
+  CONSTRAINT `fk_fornecedor_endereco1` FOREIGN KEY (`endereco_cep`) REFERENCES `enderecos` (`cep`)
+);
+
+CREATE TABLE IF NOT EXISTS `veiculos` (
+  `id_veiculo` BIGINT NOT NULL,
+  `modelo` VARCHAR(45) NOT NULL,
+  `novos` TINYINT NOT NULL,
+  `eletrico` TINYINT NOT NULL,
+  `esportivo` TINYINT NOT NULL,
+  `picape` TINYINT NOT NULL,
+  `ano` INT NOT NULL,
+  `cor` VARCHAR(45) NOT NULL,
+  `marca` VARCHAR(45) NOT NULL,
+  `preco` DOUBLE NOT NULL,
+  `quilometragem` FLOAT NOT NULL,
+  `potencia` INT NOT NULL,
+  `abs` TINYINT NOT NULL,
+  `fornecedor_id_fornecedor` BIGINT NOT NULL,
+  PRIMARY KEY (`id_veiculo`),
+  CONSTRAINT `fk_veiculos_fornecedor1` FOREIGN KEY (`fornecedor_id_fornecedor`) REFERENCES `fornecedores` (`id_fornecedor`)
+);
+
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `id_cliente` BIGINT NOT NULL,
+  `nome` VARCHAR(45) NOT NULL,
+  `cpf` BIGINT NOT NULL,
+  `usuario` VARCHAR(45) NOT NULL,
+  `telefone` BIGINT NOT NULL,
+  `gmail` VARCHAR(45) NOT NULL,
+  `table1_cep` BIGINt NOT NULL,
+  PRIMARY KEY (`id_cliente`),
+  CONSTRAINT `fk_clientes_table11` FOREIGN KEY (`table1_cep`) REFERENCES `enderecos` (`cep`)
+);
+
+CREATE TABLE IF NOT EXISTS `funcionarios` (
+  `matricula` BIGINT NOT NULL,
+  `nome` VARCHAR(45) NOT NULL,
+  `cpf` BIGINT NOT NULL,
+  `contato` BIGINT NOT NULL,
+  `salario` DOUBLE NOT NULL,
+  `funcao` VARCHAR(45) NOT NULL,
+  `enderecos_cep` BIGINT NOT NULL,
+  PRIMARY KEY (`matricula`),
+  CONSTRAINT `fk_funcionarios_enderecos1` FOREIGN KEY (`enderecos_cep`) REFERENCES `enderecos` (`cep`)
+);
+
+CREATE TABLE IF NOT EXISTS `vendidos` (
+  `veiculos_id_veiculo` BIGINT NOT NULL,
+  `clientes_id_cliente` BIGINT NOT NULL,
+  `funcionarios_matricula` BIGINT NOT NULL,
+  `nome` VARCHAR(45) NOT NULL,
+  `cpf` BIGINT NOT NULL,
+  `usuario` VARCHAR(45) NOT NULL,
+  `ipva_pago` TINYINT NOT NULL,
+  `modelo` VARCHAR(45) NOT NULL,
+  `tipo_veiculo` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (
+    `veiculos_id_veiculo`,
+    `clientes_id_cliente`,
+    `funcionarios_matricula`
+  ),
+  CONSTRAINT `fk_carros_has_clientes_carros1` FOREIGN KEY (`veiculos_id_veiculo`) REFERENCES `veiculos` (`id_veiculo`),
+  CONSTRAINT `fk_carros_has_clientes_clientes1` FOREIGN KEY (`clientes_id_cliente`) REFERENCES `clientes` (`id_cliente`),
+  CONSTRAINT `fk_vendidos_funcionarios1` FOREIGN KEY (`funcionarios_matricula`) REFERENCES `funcionarios` (`matricula`)
+);
